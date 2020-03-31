dc2ede233e6b89791623767a8626110be3d7203b
Anilton <aniltonmagalhaes@gmail.com>
2020-03-31 10:54:37 -0300

    Modificando comentário

--- scripts/aula01/desafio_batimento.js
+++ scripts/aula01/desafio_batimento.js
@@ -1,16 +1,5 @@
 // Desafio: dada a sua idade, quantas vezes seu coração já bateu?
 
-/**
- * Aula 01 - https://github.com/newtmagalhaes/QuaentenaDev/tree/master/aulas/aula01.md
- *  O calculo das batidas:
- *      - O nosso coração bate uma média de 75 vezes por minuto aproximadamente;
- *      - Sabemos que 1 hora tem 60 minutos;
- *      - Sabemos quantas horas tem o dia (24);
- *      - Sabemos quantos dias o ano tem (365, desconsiderando anos bissextos);
- * 
- * 365 * 24 * 60 * 75 = 39420000
-*/
-
 /** Retorna a estimativa de quantas vezes seu coração já bateu em função da sua idade */
 function batimentos(idade) { return idade * 365 * 24 * 60 * 75 }
 
