d18d381fe7bedf9e5f0bed96f490c8e6752de859
Anilton <aniltonmagalhaes@gmail.com>
2020-04-03 14:01:05 -0300

    Adicionando exercício da aula 04

---
+++ aulas/aula04/exercicio_custo.js
@@ -0,0 +1,7 @@
+// Desafio: como deixar as opções de distância e tempo para quem for usar o nosso programa preencher e descobrir o valor da sua corrida?
+
+const distancia = 20,
+      tempo = 60,
+      custo = 2 + distancia * 1.4 + tempo * 0.2;
+
+console.log("O custo é:", custo)
